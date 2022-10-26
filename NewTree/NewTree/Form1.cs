9c00ee46a541ae2402f3920fb13928f8fc9e2a5c
HelpMySoul <ShKYu.21@uni-dubna.ru>
2022-10-26 18:30:18 +0300

    Tree added

---
+++ NewTree/NewTree/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewTree
+{
+    public partial class Form1 : Form
+    {
+        BinTree<int> MyTree = new BinTree<int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyTree.Insert(Convert.ToInt32(numericUpDown1.Value));
+            if (MyTree.Count != 0)
+            {
+                richTextBox1.Text = MyTree.Show();
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MyTree.Count != 0)
+            {
+            try
+            {
+                textBox1.Text = MyTree.Find(Convert.ToInt32(textBox1.Text)).ToString();
+            }
+            catch (Exception ex)
+            {
+                textBox1.Text = ex.Message;
+            }
+            
+                richTextBox1.Text = MyTree.Show();
+            }
+        }
+    }
+}
